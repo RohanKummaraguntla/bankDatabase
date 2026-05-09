2ef9a7b4de1370ea6623305b33c6e007463315c2
Rohan Kummaraguntla <69653360+RohanKummaraguntla@users.noreply.github.com>
2026-05-09 17:20:09 -0500

    added UI

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import sqlite3
 
 def setup_database():
-    set = sqlite3.connect('bank.db')
-    cursor = set.cursor()
+    set = sqlite3.connect('bank.db')  
+    cursor = set.cursor() 
 
-    cursor.execute('''
+    cursor.execute(''' 
     CREATE TABLE IF NOT EXISTS users(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,  
+        id INTEGER PRIMARY KEY AUTOINCREMENT,   
         username TEXT, 
-        password TEXT, 
+        password TEXT,  
         job TEXT
-    )
+    ) 
     ''')
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS accounts( 
         accountId INTEGER PRIMARY KEY AUTOINCREMENT,
-        userId INTEGER,
+        userId INTEGER, 
         balance REAL
     )
     '''
@@ -35,17 +35,17 @@ def testinginfo():
     #cursor.execute("SELECT * FROM accounts")
     #print("Accounts table:", cursor.fetchall())
     set.commit()
-    set.close()
+    set.close() 
 
 def check_balance(accountId):
-    set=sqlite3.connect('bank.db')
+    set=sqlite3.connect('bank.db') 
     cursor=set.cursor()
     cursor.execute("SELECT balance FROM accounts WHERE accountId = ?", (accountId,))
     result=cursor.fetchone()
-    set.close()
+    set.close() 
     #return result[0]
 
-    if result is not None:
+    if result is not None: 
         return result[0]
     else:
         return None
@@ -53,40 +53,40 @@ def check_balance(accountId):
 
 def deposit(accountId, amount):
     set=sqlite3.connect('bank.db')
-    cursor=set.cursor()
+    cursor=set.cursor() 
     cursor.execute("UPDATE accounts SET balance = balance + ? WHERE accountId = ?", (amount, accountId))
     set.commit()
-    set.close()
+    set.close()  
 
 
 def withdraw(accountId, amount):
-    set=sqlite3.connect('bank.db')
+    set=sqlite3.connect('bank.db') 
     cursor=set.cursor()
     cursor.execute("UPDATE accounts SET balance = balance - ? WHERE accountId = ?",(amount, accountId))
-    set.commit()
+    set.commit() 
     set.close()
 
 
 
 def create_account(userId):
-    set=sqlite3.connect('bank.db')
+    set=sqlite3.connect('bank.db')  
     cursor=set.cursor()
     cursor.execute("INSERT INTO accounts (userId, balance) VALUES (?, ?)",(userId, 0))
-    set.commit()
+    set.commit()  
     set.close()
 
-def delete_account(accountId):
+def delete_account(accountId): 
     set=sqlite3.connect('bank.db')
-    cursor=set.cursor()
+    cursor=set.cursor()  
     cursor.execute("DELETE FROM accounts WHERE accountId = ?", (accountId,))
     set.commit()
-    set.close()
+    set.close() 
 
 def modify_account(accountId, new_balance):
-    set = sqlite3.connect('bank.db')
-    cursor = set.cursor()
+    set = sqlite3.connect('bank.db') 
+    cursor = set.cursor() 
     cursor.execute( "UPDATE accounts SET balance = ? WHERE accountId = ?", (new_balance, accountId) )
-    set.commit()
+    set.commit() 
     set.close()
 
 
@@ -98,13 +98,54 @@ def get():
     result =cursor.fetchone()
     set.close()
     return result[0]
+
+#login informatoin
+def login():
+    set = sqlite3.connect('bank.db')
+    cursor = set.cursor()
+    username = input("Username: ")
+    password = input("Password: ")
+
+    cursor.execute("SELECT id, job FROM users WHERE username = ? AND password = ?", (username, password))
+    
+    res = cursor.fetchone()
+    set.close()
+
+    return res  
+
+
+#menu
+def user_menu(userId):
+    while True:
+        print("1.Check Balance") 
+        print("2.Deposit")
+        print("3.Withdraw") 
+        print("4.Exit") 
+        choice = input("Choose: ")
+
+        account = get()
+
+        if choice=="1":
+            print("Balance:", check_balance(account))
+        elif choice=="2":
+            amt = float(input("Amount: "))
+            deposit(account, amt)
+
+        elif choice=="3":
+            amt = float(input("Amount: ")) 
+            withdraw(account, amt)
+        elif choice=="4":
+            break 
+
+#testing. 
 if __name__ == "__main__":
     setup_database()
-    #res = get()
     testinginfo()
-    res = get()
-    print("Before money ", check_balance(res))
-    deposit(res, 100)
-    print("After money", check_balance(res))
-    withdraw(res, 50)
-    print("After taken money out ", check_balance(res))
+
+    user = login()
+
+    if user is None:
+        print("Invalid login")
+    else:
+        user_id, role = user
+        user_menu(user_id)
